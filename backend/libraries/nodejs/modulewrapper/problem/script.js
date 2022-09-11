72251c2fe987202a513dcd6f645d4c42daa9c8c9
raghum99 <raghum9@icloud.com>
2022-09-11 17:46:19 +0530

    jnbjk

---
+++ backend/libraries/nodejs/modulewrapper/problem/script.js
@@ -0,0 +1,24 @@
+(function(exports, require, module, __filename, __dirname) { 
+    // Module code actually lives in here 
+});
+
+
+
+// let adress = '/Library/My Documents/FullStackDev/Project2/backend/libraries/nodejs/modulewrapper/problem/hello.html'
+// console.log (__dirname, __filename, exports, require)
+// ((__dirname, __filename, exports, require)=>{
+
+// })()
+
+
+    
+    var url = '/Library/My Documents/FullStackDev/Project2/backend/libraries/nodejs/modulewrapper/problem/hello.html';
+    console.log(__filename);
+    console.log(__dirname);
+
+
+    // function log (message) {
+    //     console.log(message);
+    // }
+
+    // module.exports = log;
